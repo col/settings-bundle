f34a0a70da1f15f4d6ca695fe9cfa15bcfc49438
Jimmy Dee <jgvdthree@gmail.com>
2017-06-09 15:28:06 -0700

    corrected method definition inference

--- lib/fastlane/plugin/settings_bundle/helper/settings_bundle_helper.rb
+++ lib/fastlane/plugin/settings_bundle/helper/settings_bundle_helper.rb
@@ -131,7 +131,7 @@ module Fastlane
           # Save (raises)
           Plist::Emit.save_plist settings_plist, plist_path
 
-          Actions.add_modified_files [plist_path] if defined?(Actions.modified_files)
+          Actions.add_modified_files [plist_path] if defined?(Actions.add_modified_files)
         end
       end
     end
